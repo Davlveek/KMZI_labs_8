be1bb4548aef8b1307118b746ec2d5f509bbf0dc
davlveek <stankeev.vladimir@gmail.com>
2020-03-14 04:01:11 +0300

    Main and RSA class skeletons

---
+++ lab_1/main.py
@@ -0,0 +1,20 @@
+from aes import AESCipher
+from rsa import RSA
+from parse import parse_arguments
+
+
+def encrypt():
+    pass
+
+
+def decrypt():
+    pass
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    if args.encrypt:
+        pass
+    elif args.decrypt:
+        pass
---
+++ lab_1/rsa.py
@@ -0,0 +1,8 @@
+class RSA:
+    @staticmethod
+    def encrypt():
+        pass
+
+    @staticmethod
+    def decrypt():
+        pass
