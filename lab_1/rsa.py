3725ee72686cda921f9910b58de9500336a0fd5f
davlveek <stankeev.vladimir@gmail.com>
2020-03-14 17:42:41 +0300

    RSA encrypt and decrypt methods

--- lab_1/rsa.py
+++ lab_1/rsa.py
@@ -1,8 +1,8 @@
 class RSA:
     @staticmethod
-    def encrypt():
-        pass
+    def encrypt(m, e, n):
+        return pow(m, e, n)
 
     @staticmethod
-    def decrypt():
-        pass
+    def decrypt(c, d, n):
+        return pow(c, d, n)
