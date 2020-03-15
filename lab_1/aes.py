1e44f8f9392bcf9a58f4453fef5a09473fc54326
davlveek <stankeev.vladimir@gmail.com>
2020-03-16 02:54:09 +0300

    Const right view

--- lab_1/aes.py
+++ lab_1/aes.py
@@ -2,19 +2,20 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
-iv = b'\x00' * AES.block_size
+IV = b'\x00' * AES.block_size
+AES_KEY_SIZE = 32
 
 
 class AESCipher:
     @staticmethod
     def encrypt(data):
-        key = get_random_bytes(AES.key_size[-1])  # Key size is 32
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        key = get_random_bytes(AES_KEY_SIZE)
+        cipher = AES.new(key, AES.MODE_CBC, IV)
         ciphertext = cipher.encrypt(pad(data, AES.block_size))
         return key, ciphertext
 
     @staticmethod
     def decrypt(ciphertext, key):
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_CBC, IV)
         data = unpad(cipher.decrypt(ciphertext), AES.block_size)
         return data
