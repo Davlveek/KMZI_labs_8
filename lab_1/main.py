8b37f1190510de2b8e0304d9f0bc100731aac877
davlveek <stankeev.vladimir@gmail.com>
2020-03-16 03:10:17 +0300

    RSA decrypting and encrypting

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,8 +1,16 @@
-from aes import AESCipher
+from aes import AESCipher, AES_KEY_SIZE
 from rsa import RSA
 from asn import ASNCoder
 from parse import parse_arguments
-import struct
+from privkey import *
+
+PRIVKEY_FILE = 'privkey.py'
+
+
+def form_privkey_file(d):
+    params = f'privkey = {str(d)}\n'
+    with open(PRIVKEY_FILE, 'w') as params_file:
+        params_file.write(params)
 
 
 def encrypt(filename):
@@ -10,9 +18,24 @@ def encrypt(filename):
         data = file.read()
         key, ciphertext = AESCipher.encrypt(data)
 
+        e, d, n = RSA.generate_params()
+        form_privkey_file(d)
+        encrypted_key = RSA.encrypt(int.from_bytes(key, 'big'), e, n)
+        asn_encoded = ASNCoder.encode(n, e, encrypted_key, ciphertext)
 
-def decrypt():
-    pass
+        with open(f'{filename}.asn1', 'wb') as asn_file:
+            asn_file.write(asn_encoded)
+
+
+def decrypt(filename):
+    n, e, encrypted_key, ciphertext = ASNCoder.decode(filename)
+    key = RSA.decrypt(encrypted_key, privkey, n)
+    plaintext = AESCipher.decrypt(
+        ciphertext,
+        key.to_bytes(AES_KEY_SIZE, 'big')
+    )
+    with open(f'{filename}.dec', 'wb') as file:
+        file.write(plaintext)
 
 
 if __name__ == '__main__':
@@ -20,4 +43,4 @@ if __name__ == '__main__':
     if args.encrypt:
         encrypt(args.file)
     elif args.decrypt:
-        pass
+        decrypt(args.file)
