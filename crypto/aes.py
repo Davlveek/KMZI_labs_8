4175d2a020477b34b76baf620b107de69d4e9675
davlveek <stankeev.vladimir@gmail.com>
2020-03-25 18:26:44 +0300

    AES encryption method without generating key

--- crypto/aes.py
+++ crypto/aes.py
@@ -15,6 +15,12 @@ class AESCipher:
         return key, ciphertext
 
     @staticmethod
+    def encrypt_with_key(data, key):
+        cipher = AES.new(key.to_bytes(AES_KEY_SIZE, 'big'), AES.MODE_CBC, IV)
+        ciphertext = cipher.encrypt(pad(data, AES.block_size))
+        return ciphertext
+
+    @staticmethod
     def decrypt(ciphertext, key):
         cipher = AES.new(key, AES.MODE_CBC, IV)
         data = unpad(cipher.decrypt(ciphertext), AES.block_size)
